9e8fc98a910ce28cdc02e9af0f53f40fea4cf446
KwonEna <eeena214@gmail.com>
2021-09-11 02:06:48 +0900

    Fix : 텍스트 미입력시 생기는 오류 수정, 미입력시 alert창 생성

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const plus = document.querySelector("#plusBtn");
 
 function inputText() {
   let text = document.querySelector("#textBox").value;
-  // div에 리스트 추가
   let div = document.createElement("div", "listText");
   const listBox = document.querySelector(".listBox");
-  // li.setAttribute('class', 'list');
-  // li.innerText = text;
-  // ul.appendChild(li);
+
+  // 텍스트 미입력시 입력하라는 경고창
+  if (text.trim() == "") {
+    alert("텍스트를 입력해 주세요!");
+    return 0;
+  }
+
+  // 텍스트 입력시 리스트에 추가
   div.innerHTML = `
             <span class="list">
             ${text}<img src="./img/delete.png"
@@ -20,13 +24,17 @@ function inputText() {
   document.querySelector("#textBox").value = "";
 }
 
+// plus 버튼 클릭시 텍스트 입력
 plus.addEventListener("click", () => {
   inputText();
 });
 
+// 엔터키 클릭시 텍스트 입력
 function pressEnter() {
   if (window.event.keyCode == 13) inputText();
 }
+
+// x버튼 이미지 클릭시 리스트 제거
 function removeList() {
   let list = document.querySelector(".list");
   list.remove();
